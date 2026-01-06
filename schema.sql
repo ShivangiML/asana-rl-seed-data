87c8784f828f9c812f6b31123023a0eb308a5bba
Shivangi Mehta <shivangimehta197@gmail.com>
2026-01-06 08:09:54 +0000

    Add organizations table

--- schema.sql
+++ schema.sql
@@ -1,7 +1,14 @@
 -- Asana RL Environment Schema
 -- Simulated B2B SaaS company (~7000 employees)
 
--- Organizations / Workspaces
+-- Organizations / Workspaces 
+CREATE TABLE organizations (
+    organization_id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    domain TEXT,
+    created_at TIMESTAMP NOT NULL
+);
+
 -- Users
 -- Teams
 -- Team Memberships
